f2f1505f560c8e0078eb1f2b82cbee466c95f999
djimenezc-code <djimenezc@utem.cl>
2026-05-06 14:12:18 -0400

    modificacion index

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
 }));
 
 const storeInfo = {
-    direccion: "Av San Martin, Talagante",
+    direccion: "Av San Martin 0443, Talagante",
     horario: "Lun-Sáb: 08:30 - 20:30 | Dom: 10:00 - 15:00",
     telefono: "+56 9 48 539049",
     mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3321.43633880629!2d-70.930419!3d-33.665219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzPCsDM5JzU0LjgiUyA3MMKwNTUnNDkuNSJX!5e0!3m2!1ses!2scl!4v1617000000000!5m2!1ses!2scl" 
